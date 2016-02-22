0841404e4512d23af666e6f72ace66f22f96f71c
Sergey Sobko <S.Sobko@profitware.ru>
2016-02-22 14:18:14 +0300

    clean xmonad config code

--- xmonad-fedora.hs
+++ xmonad-fedora.hs
@@ -1,55 +1,57 @@
 -- PrDE XMonad Config
 
-import System.Posix.Env (getEnv, putEnv)
-import Data.Maybe (maybe)
 import Control.Monad
-import XMonad.Hooks.SetWMName
-import qualified XMonad.StackSet as W
-import XMonad.Util.EZConfig
-import XMonad.Util.Run
-import qualified Data.Map        as M 
+import Data.Maybe (maybe)
+import System.Posix.Env (getEnv, putEnv)
 
 import XMonad
 import XMonad.Config.Desktop
 import XMonad.Config.Gnome
 import XMonad.Config.Kde
 import XMonad.Config.Xfce
+import XMonad.Hooks.SetWMName
+import XMonad.Util.EZConfig
+import XMonad.Util.Run
 
-myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $           
-    -- launch a terminal                                               
-    [ ((modm, xK_Return), spawn $ XMonad.terminal conf)                
-    -- launch dmenu                                                    
-    -- , ((modm,               xK_p     ), spawn myDmenuCommand)  
-                                                                       
-    -- close focused window                                            
-    , ((modm .|. shiftMask, xK_c     ), kill) 
+import qualified Data.Map as M
+import qualified XMonad.StackSet as W
 
-    , ((modm,               xK_Tab   ), windows W.focusDown)           
-                                                                       
-    -- Move focus to the next window                                   
-    , ((modm,               xK_j     ), windows W.focusDown)           
-                                                                       
-    -- Move focus to the previous window                               
-    , ((modm,               xK_k     ), windows W.focusUp  )           
-                                                                       
-    -- Move focus to the master window                                 
-    , ((modm,               xK_m     ), windows W.focusMaster  ) 
 
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+
+    -- launch a terminal
+    [ ((modm, xK_Return), spawn $ XMonad.terminal conf)
+
+    -- close focused window
+    , ((modm .|. shiftMask, xK_c), kill)
+    , ((modm,xK_Tab), windows W.focusDown)
+
+    -- Move focus to the next window
+    , ((modm, xK_j), windows W.focusDown)
+
+    -- Move focus to the previous window
+    , ((modm, xK_k), windows W.focusUp)
+
+    -- Move focus to the master window
+    , ((modm, xK_m), windows W.focusMaster)
+
+    -- Launch Emacs client
     , ((modm, xK_e), spawn "emacsclient -c")
+
     --, ((modm, xK_Left), prevWS)
     --, ((modm, xK_Right), nextWS)
 
-    --, ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess)) 
-                                                                       
-    -- Restart xmonad                                                  
-    -- , ((modm              , xK_q     ), restart "xmonad" True)
+    --, ((modm .|. shiftMask, xK_q), io (exitWith ExitSuccess))
+
+    -- Restart xmonad
+    , ((modm, xK_q), restart "xmonad" True)
 
     , ((modm .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
     ]
     ++
 
     [((m .|. modm, k), windows $ f i)    
-        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]        
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
 
 
